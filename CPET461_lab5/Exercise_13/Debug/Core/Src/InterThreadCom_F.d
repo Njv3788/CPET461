34a7c7e9744851993bfd11f6035e5f5807a198a8
Nathaniel Valla (RIT Student) <njv3788@rit.edu>
2024-03-20 17:00:04 -0400

    Exercise 17 done

--- CPET461_lab5/Exercise_13/Debug/Core/Src/InterThreadCom_F.d
+++
@@ -1,4 +0,0 @@
-Core/Src/InterThreadCom_F.o: ../Core/Src/InterThreadCom_F.c \
- ../Core/Inc/InterThreadCom_F.h
-
-../Core/Inc/InterThreadCom_F.h:
--- CPET461_lab5/Exercise_13/Debug/Core/Src/MY_FLAG.d
+++
@@ -1,3 +0,0 @@
-Core/Src/MY_FLAG.o: ../Core/Src/MY_FLAG.c ../Core/Inc/MY_FLAG.h
-
-../Core/Inc/MY_FLAG.h:
